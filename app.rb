92c50677ee3291dcde87b3bf5415270a62906630
Michal Cichra <michal@o2h.cz>
2013-10-24 17:10:53 +0200

    use brainslug

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require 'airbrake'
 
 Airbrake.configure do |config|
   config.api_key = '29cfc3598cb07e8e39f596a29431ec0d'
+
+  config.proxy_host = 'localhost'
+  config.proxy_port = 10001
+
+  config.proxy_user = 'mikz'
+  config.proxy_pass = 'fsdfdsfdsfdsf'
 end
 
 class BrainslugTest < Sinatra::Base
